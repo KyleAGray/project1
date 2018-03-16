a4031b9c322d86e3463f5ca4a7a457a96f478fbb
yejianan <yejianan@EB2314P10>
2018-03-16 09:54:45 -0400

    nothing

--- Synthie/PianoFactory.cpp
+++ Synthie/PianoFactory.cpp
@@ -36,7 +36,7 @@ void CPianoFactory::GetFiles()
 {
 	namespace stdfs = std::experimental::filesystem;
 	const stdfs::directory_iterator end{};
-	std::experimental::filesystem::path path = "Piano";
+	std::experimental::filesystem::path path = "Piano3";
 
 	for (stdfs::directory_iterator iter(path); iter != end; ++iter)
 	{
